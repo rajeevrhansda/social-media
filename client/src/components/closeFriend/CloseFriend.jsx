dfd6ad32ddd6567ce52137f99f8cea07b1328f65
Rajeev <rajeevrhansda@gmail.com>
2022-07-12 13:26:43 +0530

    PF process.env in close friend

--- client/src/components/closeFriend/CloseFriend.jsx
+++ client/src/components/closeFriend/CloseFriend.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import './closeFriend.css'
 
 export default function CloseFriend({user}) {
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
     return (
         <li className='sidebarFriend'>
-            <img className='sidebarFriendImg' src={user.profilePicture} alt="Friend" />
+            <img className='sidebarFriendImg' src={PF+user.profilePicture} alt="Friend" />
             <span className='sidebarFriendName'>{user.username}</span>
         </li>
     )
