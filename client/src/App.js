29ad7d9f198aebb811db6ead7a43f66ced3d96b9
Rajeev <rajeevrhansda@gmail.com>
2022-07-12 11:56:57 +0530

    implement react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
-
-// import Home from "./pages/home/Home";
-// import Login from "./pages/login/Login";
-// import Profile from "./pages/profile/Profile";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 
 function App() {
   return (
-    // <Home/>
-    // <Login/>
-    // <Profile/>
-    <Register/>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
   );
 }
 
