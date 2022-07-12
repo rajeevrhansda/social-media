798fd08a051b745f30bc4d7d1d2498777ea08b65
Rajeev <rajeevrhansda@gmail.com>
2022-07-13 02:02:42 +0530

    multer upload image api

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer = require('multer')
 
 dotenv.config();
 
@@ -22,6 +23,24 @@ mongoose.connect(
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, "public/images");
+    },
+    filename:(req, file, cb)=>{
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({storage});
+app.post('/api/upload', upload.single("file"), (req, res)=>{
+    try{
+        return res.status(200).json("File uploaded successfully ")
+    }catch(err){
+
+    }
+})
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
